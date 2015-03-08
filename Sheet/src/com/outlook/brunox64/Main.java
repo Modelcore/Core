6e8fab163d03467321a310a40639db4710f33903
Bruno Moreira Mota <brunox64@outlook.com>
2015-03-08 15:22:43 -0300

    outra

--- Sheet/src/com/outlook/brunox64/Main.java
+++ Sheet/src/com/outlook/brunox64/Main.java
@@ -2,6 +2,7 @@ package com.outlook.brunox64;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.URL;
 
 import javax.json.Json;
 import javax.json.JsonArray;
@@ -15,7 +16,10 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.bson.Document;
 
+import com.google.gdata.client.spreadsheet.SpreadsheetQuery;
 import com.google.gdata.client.spreadsheet.SpreadsheetService;
+import com.google.gdata.data.spreadsheet.SpreadsheetEntry;
+import com.google.gdata.data.spreadsheet.SpreadsheetFeed;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
@@ -90,12 +94,43 @@ public class Main extends HttpServlet {
 	}
 	
 	private void load() throws Exception {
-		String clientId = "724005170397-720qiupce61tfpem214phaqb2k79l2lu.apps.googleusercontent.com";
-		String email = "724005170397-720qiupce61tfpem214phaqb2k79l2lu@developer.gserviceaccount.com";
-		String filePrivateKey = "META-INF/APIProject-6c04961f1256.p12";
+//		String clientId = "724005170397-720qiupce61tfpem214phaqb2k79l2lu.apps.googleusercontent.com";
+//		String clientSecret = "notasecret";
+//		
+//		String accountId = "724005170397-720qiupce61tfpem214phaqb2k79l2lu@developer.gserviceaccount.com";
+//		
+//		String accountUser = "brunx64@gmail.com";
+//		
+//		String filePrivateKey = "META-INF/ProjetoSheet-f0aa3177d268.p12";
+//		String sheetScopeFull = "https://spreadsheets.google.com/feeds/spreadsheets/private/basic";
+//		
+//		HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+//		JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+//		
+//		GoogleCredential credentials = new GoogleCredential.Builder()
+//			.setServiceAccountId(accountId)
+//			.setServiceAccountPrivateKeyFromP12File(new File(filePrivateKey))
+//			.setServiceAccountUser(accountUser)
+//			.setServiceAccountScopes(Collections.singleton(sheetScopeFull))
+//			.setClientSecrets(clientId, clientSecret)
+//			.setTransport(httpTransport)
+//			.setJsonFactory(jsonFactory)
+//		
+//			.build();
+//		
 		
 		SpreadsheetService service = new SpreadsheetService("ProjetoSheet");
-		service.setUserCredentials("brunx64@gmail.com", "Bruno@x64");
+		//service.setOAuth2Credentials(credentials);
+		service.setUserCredentials("brunowsws@gmail.com", "senhadaplanilha");
+		
+		URL SPREADSHEET_FEED_URL = new URL("https://spreadsheets.google.com/feeds/spreadsheets/private/full");
+
+		SpreadsheetQuery query = new SpreadsheetQuery(SPREADSHEET_FEED_URL);
+		query.setTitleQuery("Planilha01");
+		query.setTitleExact(true);
+		SpreadsheetFeed feed = service.getFeed(query, SpreadsheetFeed.class);
+		SpreadsheetEntry entry = feed.getEntries().get(0);
+		
 		
 	}
 	
